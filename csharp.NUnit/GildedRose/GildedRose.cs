35a4d05e2195c22bf0036bdeab1251b8b0e68020
Carlos Da Silva <carloshdezdasilva@gmail.com>
2026-05-20 21:01:16 +0100

    refactor: Isolate Legendary Items behavior in UpdateQuality

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -91,14 +91,16 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
+            if (Items[i].Name.Equals("Sulfuras, Hand of Ragnaros"))
+            {
+                continue;
+            }
+            
             if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
             {
                 if (Items[i].Quality > 0)
                 {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
+                    Items[i].Quality = Items[i].Quality - 1;
                 }
             }
             else
@@ -127,12 +129,9 @@ public class GildedRose
                     }
                 }
             }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
-
+            
+            Items[i].SellIn = Items[i].SellIn - 1;
+            
             if (Items[i].SellIn < 0)
             {
                 if (Items[i].Name != "Aged Brie")
