caef7412252b2a338374a5c4e308502bad8008d1
Carlos Da Silva <carloshdezdasilva@gmail.com>
2026-05-20 02:12:24 +0100

    fix: Fix test arrange to properly test that quality decrease

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -9,7 +9,7 @@ public class GildedRoseTest
     [Test]
     public void AnItemQualityDecreaseWhenUpdateQualityIsCalled()
     {
-        var items = new List<Item> { new Item { Name = "An Item", SellIn = 2, Quality = 0 } };
+        var items = new List<Item> { new Item { Name = "An Item", SellIn = 2, Quality = 1 } };
         var app = new GildedRose(items);
         
         app.UpdateQuality();
