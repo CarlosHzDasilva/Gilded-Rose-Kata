ff1918fce08af80729ca766a56d1d45afcd4a165
Carlos Da Silva <carloshdezdasilva@gmail.com>
2026-05-20 21:13:30 +0100

    refactor: Isolate items behavior after SellIn update

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -138,30 +138,35 @@ public class GildedRose
             
             Items[i].SellIn = Items[i].SellIn - 1;
             
-            if (Items[i].SellIn < 0)
+            if (Items[i].Name.Equals("Aged Brie"))
             {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
+                if (Items[i].SellIn < 0)
                 {
                     if (Items[i].Quality < 50)
                     {
                         Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+            }
+
+            if (Items[i].Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+            {
+                if (Items[i].SellIn < 0)
+                {
+                    Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                }
+            }
+                
+            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
                     }
                 }
             }
